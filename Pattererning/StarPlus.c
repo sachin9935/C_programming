8327150df6396c8df601aaf79e36fe22ea07c359
Sachin Mishra <152723124+sachin9935@users.noreply.github.com>
2024-03-10 09:32:03 +0530

    Pattern printing Questions

---
+++ Pattererning/StarPlus.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int n;
+    printf("enter number...");
+    scanf("%d",&n);
+    for(int i=1; i<=n; i++){
+        int a=65;
+        for(int j=1; j<=i; j++){
+            printf("%c",a);
+            a++;
+        }
+        printf("\n");
+    }
+}
