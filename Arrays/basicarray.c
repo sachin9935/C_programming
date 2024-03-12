4c54ae3cfcd7896bdd60a731651db315c9a833b1
Sachin Mishra <152723124+sachin9935@users.noreply.github.com>
2024-03-12 21:21:18 +0530

    Arrays Questions

---
+++ Arrays/basicarray.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int arr3[5];
+    for (int i = 0; i <= 4; i++)
+    {
+        printf("enter the element number %d\n", i + 1);
+        scanf("%d", &arr3[i]);
+    }
+    for (int i = 0; i <= 4; i++)
+    {
+        printf("%d ", arr3[i]);
+    }
+    return 0;
+}
