c6862318c23acffe06ae4bb55fa195e13fa6d6f3
Sachin Mishra <152723124+sachin9935@users.noreply.github.com>
2024-03-14 19:55:12 +0530

    Array with loop

---
+++ Arrays/countthenumber.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    int arr[4]={1,9,3,7};
+    int x=4;
+    int count = 0;
+    for(int i=0; i<=3; i++){
+        if(arr[i]>x){
+            count++;
+        }
+       
+    }
+     printf("%d",count);
+    // int n;
+    // printf("enter number");
+    // scanf("")
+}
+
