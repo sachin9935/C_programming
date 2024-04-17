83528b6e0bec0dfe131a26c3660d28943a7e365d
Sachin Mishra <152723124+sachin9935@users.noreply.github.com>
2024-04-17 13:15:54 +0530

    Fibonacci numbers

---
+++ Recursion/fiborec.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int fibo(int n){
+    if(n<=2) return 1;
+    return fibo(n-1)+fibo(n-2);
+}
+
+int main(){
+    int n;
+    printf("enter number:");
+    scanf("%d",&n);
+    printf("%d",fibo(n));
+    return 0;
+}
