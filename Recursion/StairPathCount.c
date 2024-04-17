7d4d5b843c4c506538c3c45d82d3c22195934e6e
Sachin Mishra <152723124+sachin9935@users.noreply.github.com>
2024-04-17 13:17:19 +0530

    Counting the total Possible straight path.

---
+++ Recursion/StairPathCount.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int stair(int n){
+    if(n<=2) return n;
+    int totalways=stair(n-1)+stair(n-2);
+    return totalways;
+}
+
+int main(){
+    int n;
+    printf("enter number:");
+    scanf("%d",&n);
+    printf("%d",stair(n));
+    return 0;
+}
