925c8842b7a0bdc9143468c9d146ec8151842f03
Sachin Mishra <152723124+sachin9935@users.noreply.github.com>
2024-04-19 22:04:39 +0530

    zigzag using (preinpost)

---
+++ Recursion/zigzag.c
@@ -0,0 +1,19 @@
+
+#include <stdio.h>
+
+void zigzag(int n) {
+    if (n == 0) return;
+    printf("%d ", n);
+    zigzag(n - 1);
+    printf("%d ", n);
+    zigzag(n - 1);
+    printf("%d ", n);
+}
+
+int main() {
+    int n;
+    printf("Enter number:");
+    scanf("%d", &n);
+    zigzag(n);
+    return 0;
+}
